0af9880f52cd3902ad7034ae946e5a2db9b4d837
Eviber <youv.didj@gmail.com>
2022-04-02 22:37:47 +0200

    fix width

--- userscript.user.js
+++ userscript.user.js
@@ -21,7 +21,7 @@ if (window.top !== window.self) {
             i.src =
               "https://raw.githubusercontent.com/Eviber/42_place/main/template_42.png";
             i.style =
-              "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+              "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
           })()
