ec5aa6c5c3762f5b6d71bb9eb952f31be64542c6
Eviber <youv.didj@gmail.com>
2022-04-04 00:47:51 +0200

    4X template

--- userscript.user.js
+++ userscript.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         42 Logo template
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.5
 // @description  try to take over the canvas!
-// @author       oralekin, exdeejay (xDJ_)
+// @author       Eviber
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @updateURL    https://github.com/Eviber/42_place/raw/main/userscript.user.js
+// @downloadURL  https://github.com/Eviber/42_place/raw/main/userscript.user.js
 // @grant        none
 // ==/UserScript==
 if (window.top !== window.self) {
@@ -21,7 +23,7 @@ if (window.top !== window.self) {
             i.src =
               "https://raw.githubusercontent.com/Eviber/42_place/main/template_42.png";
             i.style =
-              "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+              "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
           })()
